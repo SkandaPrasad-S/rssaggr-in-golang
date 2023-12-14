86a07e996784298bf156c7c3382c11aaa57650ad
SkandaPrasad-S <skandaprasad98@gmail.com>
2023-12-14 15:37:53 +0530

    Added boiler plate chi router code

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	// "fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	portString := os.Getenv("Port")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the env")
+	}
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"}, // Add your allowed origins here
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+
+	v1Router.Get("/err",handlerErr)
+
+	router.Mount("/v1", v1Router)
+	log.Printf("Starting server here on port %v",portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
